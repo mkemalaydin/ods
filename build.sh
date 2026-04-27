0c9cb11d9041fec1283ea3156714bd0694c77b9a
mkemalaydin <kemal06@gmail.com>
2026-04-27 11:20:22 +0000

    Fix build.sh: export DJANGO_SETTINGS_MODULE for proper migration execution

--- build.sh
+++ build.sh
@@ -12,8 +12,18 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Set Django settings module
+export DJANGO_SETTINGS_MODULE=ods_app.settings
+
 # Run Django migrations
-DJANGO_SETTINGS_MODULE=ods_app.settings python manage.py migrate
+python manage.py migrate
 
 # Collect static files
-DJANGO_SETTINGS_MODULE=ods_app.settings python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
